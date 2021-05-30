1be7e3aec3e869dec5068ee5a41001a3f8adf2f1
Daniel Garcia Uscanga <dalokdaga@gmail.com>
2021-05-30 18:04:08 -0500

    avance hasta el video 162

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
+  public registerForm = this.fb.group({
+    nombre   :['Daniel',[Validators.required]],
+    email    :['test100@gmail.com', Validators.required],
+    password :['123456',[Validators.required]],
+    password2:['123456',[Validators.required]],
+    terminos:[false,[Validators.required]],
+  })
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  crearUsuario(){
+    console.log(this.registerForm.value)
   }
-
 }
